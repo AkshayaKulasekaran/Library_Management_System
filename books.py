2724d9256f1417d8cf9ab9494faeb556a042cb03
Akshaya <61582763+AkshayaKulasekaran@users.noreply.github.com>
2020-08-26 16:30:02 +0530

    Create books.py

---
+++ books.py
@@ -0,0 +1,9 @@
+# A list of books for the library management system
+books = { "Harry Potter" : 10,
+"The Story of my Experiments with Truth" : 10,
+"The In and Outs of Cyber Security" : 5,
+"The Internet of Everything" : 5,
+"Green Eggs and Ham" : 12,
+"Finding Neverland" : 12,
+"Wings of Fire" : 3,
+"Children's Fairytales" : 3 }
