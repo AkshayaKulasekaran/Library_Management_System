b4136939787a070976bb7c83b0bf78dae92180fe
Akshaya <61582763+AkshayaKulasekaran@users.noreply.github.com>
2020-11-15 21:56:16 +0530

    Update books.py

--- books.py
+++ books.py
@@ -1,13 +1,15 @@
 # A list of books for the library management system
-books = { "Harry Potter" : 10,
-"The Story of my Experiments with Truth" : 10,
-"The In and Outs of Cyber Security" : 5,
-"The Internet of Everything" : 5,
-"Green Eggs and Ham" : 12,
-"Finding Neverland" : 12,
-"Wings of Fire" : 3,
-"A Bug's Life" : 9,
-"Bilbo Baggins" : 9,
-"Grimm's Fairytales" : 3,
-"A Coder's Climb" : 0
+books = {
+  # 'book_title' : number_of_copies
+  "Harry Potter" : 10,
+  "The Story of my Experiments with Truth" : 10,
+  "The In and Outs of Cyber Security" : 5,
+  "The Internet of Everything" : 5,
+  "Green Eggs and Ham" : 12,
+  "Finding Neverland" : 12,
+  "Wings of Fire" : 3,
+  "A Bug's Life" : 9,
+  "Bilbo Baggins" : 9,
+  "Grimm's Fairytales" : 3,
+  "A Coder's Climb" : 10
 }
